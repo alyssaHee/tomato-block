21017264aed40469047874c81260b5fcd26195ff
Alyssa Hee <alyssahee43@gmail.com>
2025-08-12 11:35:25 -0500

    Update shield to fix background colour

--- Shield/ShieldConfigurationExtension.swift
+++ Shield/ShieldConfigurationExtension.swift
@@ -15,34 +15,47 @@ import UIKit
 class ShieldConfigurationExtension: ShieldConfigurationDataSource {
     override func configuration(shielding application: Application) -> ShieldConfiguration {
         return ShieldConfiguration(
-            backgroundColor: UIColor(red: 0.12, green: 0.11, blue: 0.11, alpha: 1.0),
-            icon: UIImage(named: "launchscreen"),
-            title: ShieldConfiguration.Label(text: "Good things take time", color: .white),
-            subtitle: ShieldConfiguration.Label(text: "Take a breath, reset. You got this! To access app tap your tomato brick.", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)),
-            primaryButtonLabel: ShieldConfiguration.Label(text: "Focus", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)),
-            primaryButtonBackgroundColor: UIColor(red: 0.21, green: 0.18, blue: 0.18, alpha: 1.0)
+            backgroundBlurStyle: .systemMaterialDark,
+            backgroundColor: UIColor(named: "shieldBg"),
+            icon: UIImage(named: "shieldIcon"),
+            title: ShieldConfiguration.Label(text: "Good Things Take Time", color: UIColor(named: "titleColour")!),
+            subtitle: ShieldConfiguration.Label(text: "Take a breath, reset. You got this! To access app tap your tomato brick.", color: UIColor(named: "textColour")!),
+            primaryButtonLabel: ShieldConfiguration.Label(text: "Focus", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)),
+            primaryButtonBackgroundColor: UIColor(red: 0.26, green: 0.23, blue: 0.23, alpha: 0.8)
         )
     }
     
     override func configuration(shielding application: Application, in category: ActivityCategory) -> ShieldConfiguration {
         // Customize the shield as needed for applications shielded because of their category.
         return ShieldConfiguration(
-            backgroundColor: UIColor(red: 0.12, green: 0.11, blue: 0.11, alpha: 1.0),
-            icon: UIImage(named: "launchscreen"),
-            title: ShieldConfiguration.Label(text: "Good Things Take Time", color: .white),
-            subtitle: ShieldConfiguration.Label(text: "Take a breath, reset. You got this! To access app tap your tomato brick.", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)),
-            primaryButtonLabel: ShieldConfiguration.Label(text: "Focus", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)),
+            backgroundColor: UIColor(named: "shieldBg"),
+            icon: UIImage(named: "shieldIcon"),
+            title: ShieldConfiguration.Label(text: "Good Things Take Time", color: UIColor(named: "textColour")!),
+            subtitle: ShieldConfiguration.Label(text: "Take a breath, reset. You got this! To access app tap your tomato brick.", color: UIColor(named: "textColour")!),
+            primaryButtonLabel: ShieldConfiguration.Label(text: "Focus", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)),
             primaryButtonBackgroundColor: UIColor(red: 0.21, green: 0.18, blue: 0.18, alpha: 1.0)
         )
     }
     
     override func configuration(shielding webDomain: WebDomain) -> ShieldConfiguration {
-        // Customize the shield as needed for web domains.
-        ShieldConfiguration()
+        return ShieldConfiguration(
+            backgroundColor: UIColor(named: "shieldBg"),
+            icon: UIImage(named: "shieldIcon"),
+            title: ShieldConfiguration.Label(text: "Good Things Take Time", color: UIColor(named: "textColour")!),
+            subtitle: ShieldConfiguration.Label(text: "Take a breath, reset. You got this! To access app tap your tomato brick.", color: UIColor(named: "textColour")!),
+            primaryButtonLabel: ShieldConfiguration.Label(text: "Focus", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)),
+            primaryButtonBackgroundColor: UIColor(red: 0.21, green: 0.18, blue: 0.18, alpha: 1.0)
+        )
     }
     
     override func configuration(shielding webDomain: WebDomain, in category: ActivityCategory) -> ShieldConfiguration {
-        // Customize the shield as needed for web domains shielded because of their category.
-        ShieldConfiguration()
+        return ShieldConfiguration(
+            backgroundColor: UIColor(named: "shieldBg"),
+            icon: UIImage(named: "shieldIcon"),
+            title: ShieldConfiguration.Label(text: "Good Things Take Time", color: .white),
+            subtitle: ShieldConfiguration.Label(text: "Take a breath, reset. You got this! To access app tap your tomato brick.", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)),
+            primaryButtonLabel: ShieldConfiguration.Label(text: "Focus", color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)),
+            primaryButtonBackgroundColor: UIColor(red: 0.21, green: 0.18, blue: 0.18, alpha: 1.0)
+        )
     }
 }
