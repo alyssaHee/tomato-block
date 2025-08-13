d9d4ec2e9a23e06de3305922da90bd74544c3a74
Alyssa Hee <alyssahee43@gmail.com>
2025-08-13 18:49:03 -0500

    Update timer to continue counting in background

--- Tomato Brick/Timer.swift
+++ Tomato Brick/Timer.swift
@@ -9,44 +9,65 @@ import SwiftUI
 import Foundation
 
 class TomatoTimer: ObservableObject {
-    private var startTime: Date?
+    private var timerCounting: Bool = false
     private var updateTimer: Foundation.Timer?
-
     @Published var elapsedTime: TimeInterval = 0
+    private var startTime: Date? {
+        get { UserDefaults.standard.object(forKey: "startTime") as? Date }
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: "startTime")
+        }
+    }
+    var isTimerRunning: Bool {
+        startTime != nil
+    }
+    var totalTimeBlocked: TimeInterval = 0
+    
+    init() {
+            if startTime != nil {
+                restartTimer()
+                NSLog("Restart Timer")
+            }
+        }
+    
+    private func timerProc() {
+        if let start = self.startTime {
+            self.elapsedTime = Date().timeIntervalSince(start)
+        }
+        }
+    
+    func restartTimer(){
+        self.updateTimer = Foundation.Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            self.elapsedTime = Date().timeIntervalSince(self.startTime!)
+                }
+                timerProc()
+    }
     
     func startTimer() {
+        precondition(!isTimerRunning)
         self.startTime = Date()
-        self.updateTimer = Foundation.Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
-                    self.elapsedTime = self.timeBlocked()
-                }
+        self.timerCounting = true
+        self.elapsedTime = 0
+        restartTimer()
         NSLog("Timer started")
     }
 
     func stopTimer() {
+        precondition(isTimerRunning)
+        self.totalTimeBlocked = self.elapsedTime
         self.startTime = nil
         self.updateTimer?.invalidate()
         self.updateTimer = nil
+        self.elapsedTime = 0
         NSLog("Timer stopped")
     }
 
-    func timeBlocked() -> TimeInterval {
-        return -(self.startTime?.timeIntervalSinceNow ?? 0)
-    }
 
     var formattedElapsedTime: String {
-        let totalSeconds = Int(timeBlocked())
+        let totalSeconds = Int(elapsedTime)
         let hours = totalSeconds / 3600
         let minutes = (totalSeconds % 3600) / 60
-        let seconds = (totalSeconds - hours*3600 - minutes*60)
-        if seconds < 10 && minutes < 10 {
-            return "\(hours):0\(minutes):0\(seconds)"
-        } else if seconds < 10 {
-            return "\(hours):\(minutes):0\(seconds)"
-        } else if minutes < 10 {
-            return "\(hours):0\(minutes):\(seconds)"
-        } else {
-            return "\(hours):\(minutes):\(seconds)"
-        }
-        
+        let seconds = totalSeconds % 60
+        return String(format: "%d:%02d:%02d", hours, minutes, seconds)
     }
 }
